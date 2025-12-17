612cbad856760540374167639b1cc33569470b59
Pedziwiatrr <michalozaurus@gmail.com>
2025-12-17 15:32:48 +0100

    feat: add data loader usage with test prints in main function

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import os
+from src.loader import SNDlibLoader
+
+
 def main():
-    pass
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(base_dir, 'data', 'polska.txt')
+
+    try:
+        print(f"Loading data from {file_path}")
+
+        network = SNDlibLoader.load(file_path)
+
+        print("="*100)
+        print(f"> Node count: {len(network.nodes)}")
+        print(f"> Link count: {len(network.nodes)}")
+        print(f"> Demand count: {len(network.demands)}")
+
+    except Exception as e:
+        print(f"ERROR! : {e} :(")
 
 if __name__ == "__main__":
-    pass
+    main()
