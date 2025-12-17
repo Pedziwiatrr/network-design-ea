2fb861c41c2fbc3117e87a5bf3efcd30632f8313
Pedziwiatrr <michalozaurus@gmail.com>
2025-12-17 12:08:11 +0100

    feat: add Network class and add value and allowed_paths to Demand class

--- src/models.py
+++ src/models.py
@@ -19,5 +19,24 @@ class Demand:
     id: str
     source: str
     target: str
+    value: float
+    allowed_paths: List[List[str]] = field(default_factory=list)
 
+@dataclass
+class Network:
+    nodes:
 
+@dataclass
+class Network:
+    nodes: dict[str, Node] = field(default_factory=dict)
+    links: dict[str, Link] = field(default_factory=dict)
+    demands: dict[str, Demand] = field(default_factory=dict)
+
+    def add_node(self, node: Node):
+        self.nodes[node.id] = node
+
+    def add_link(self, link: Link):
+        self.links[link.id] = link
+
+    def add_demand(self, demand: Demand):
+        self.demands[demand.id] = demand
