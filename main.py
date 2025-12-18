54106b5e38178f175649e4bb6dd7539782cff374
Pedziwiatrr <michalozaurus@gmail.com>
2025-12-18 14:46:39 +0100

    feat: add prints for evosolver's functions (temporary for testing)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import argparse
+import numpy as np
 from src.loader import SNDlibLoader
+from src.ea import EvoSolver
 
 def main():
     base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -9,6 +11,7 @@ def main():
     parser.add_argument('--nodes', action='store_true')
     parser.add_argument('--links', action='store_true')
     parser.add_argument('--demands', action='store_true')
+    parser.add_argument('--test_solver', action='store_true')
     parser.add_argument('--all', action='store_true')
     parser.add_argument('--file', type=str, default=os.path.join(base_dir, 'data', 'polska.txt'))
     args = parser.parse_args()
@@ -44,6 +47,38 @@ def main():
                 for i, path in enumerate(demand.admissable_paths):
                      print(f"   {i}: {path}")
                 print("-" * 50)
+
+        if args.test_solver or args.all:
+            print()
+            print("="*100)
+            print("< Testing evolutionary solver >\n")
+            solver = EvoSolver(network, modularity=1.0, aggregation=True)
+
+            num_demands = len(solver.demand_ids)
+            max_paths = max(len(d.admissable_paths) for d in network.demands.values())
+
+            test_individual = np.random.rand(num_demands, max_paths)
+            test_individual_2 = np.random.rand(num_demands, max_paths)
+
+            print("-" * 50)
+            print(" test get_link_loads:")
+            loads = solver.get_link_loads(test_individual)
+            print(f" all loads: {loads}")
+            total_load = sum(loads.values())
+            print(f" total load in network: {total_load:.2f}")
+
+            print("-" * 50)
+            print(" test calculate_cost:")
+            cost = solver.calculate_cost(test_individual)
+            print(f" calculated cost: {cost}")
+            print("-" * 50)
+
+            print(" test crossover")
+            child = solver.crossover(test_individual, test_individual_2)
+            print(f" crossover child: {child} ")
+            print("="*100)
+
+        print("finito")
 
     except Exception as e:
         print(f"ERROR! : {e} :(")
